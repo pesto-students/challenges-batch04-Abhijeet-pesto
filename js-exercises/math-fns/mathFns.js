e555ae4329c9cbd64e9590611b0864bcf8b3fb44
Abhijeet-pesto <abhijeet.agarwal23@gmail.com>
2020-10-22 14:29:52 +0530

    solution for math fns

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,18 @@
+function sqrt(number) {
+  return Math.sqrt(number);
+}
+
+function power(number, exponent) {
+  return number ** exponent;
+}
+
+function round(number) {
+  return Math.round(number);
+}
+
+// Don't change the exported names.
+export {
+  sqrt,
+  power,
+  round,
+};
