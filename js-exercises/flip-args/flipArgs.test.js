d5e3d253f679be5569b57a53f808afd23507dbf4
Abhijeet-pesto <abhijeet.agarwal23@gmail.com>
2020-10-22 13:26:01 +0530

    Modified flip args test file

--- js-exercises/flip-args/flipArgs.test.js
+++ js-exercises/flip-args/flipArgs.test.js
@@ -7,7 +7,7 @@ describe('sumFibsMemoized', () => {
 
   test('should return reverse of argument', () => {
     const flip = flipArgs();
-    expect(flip(1, 2, 3)).toBe([3, 2, 1]);
-    expect(flip('a', 'b', 'c', 'd')).toBe(['d', 'c', 'b', 'a']);
+    expect(flip(1, 2, 3)).toEqual([3, 2, 1]);
+    expect(flip('a', 'b', 'c', 'd')).toEqual(['d', 'c', 'b', 'a']);
   });
 });
