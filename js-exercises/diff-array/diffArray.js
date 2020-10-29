5e738e87e4e5852a850154a15ecd893acd013953
Abhijeet-pesto <abhijeet.agarwal23@gmail.com>
2020-10-29 12:49:05 +0530

    Added error handling in diffArray solution

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,6 +1,9 @@
 function diffArray(arr1, arr2) {
-  const result = [...arr1, ...arr2];
-  return result.filter(item => !(arr1.includes(item) && arr2.includes(item)));
+  if (Array.isArray(arr1) && Array.isArray(arr2)) {
+    const result = [...arr1, ...arr2];
+    return result.filter(item => !(arr1.includes(item) && arr2.includes(item)));
+  }
+  throw new Error(`Error: Expected arrays, got ${typeof (arr1)} and ${typeof (arr2)}`);
 }
 
 export {
