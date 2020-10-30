ec3557d510efa9f53ef9adc42b874b4ca5dd28e0
Abhijeet-pesto <abhijeet.agarwal23@gmail.com>
2020-10-31 01:56:33 +0530

    Solution for curry

---
+++ js-exercises/curry/curry.js
@@ -0,0 +1,12 @@
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    }
+    return (...moreArgs) => curried.apply(this, args.concat(moreArgs));
+  };
+}
+
+export {
+  curry,
+};
